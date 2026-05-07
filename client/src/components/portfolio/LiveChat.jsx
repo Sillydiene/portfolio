6063c5a75ca629e9ba09522c02487f2a5defb64f
Sillydiene <Sillydiene@gmail.com>
2026-05-07 16:46:51 -0400

    fix backend url

--- client/src/components/portfolio/LiveChat.jsx
+++ client/src/components/portfolio/LiveChat.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_BACKEND_URL, {
-    transports: ["polling"] // 🔥 IMPORTANT POUR RENDER
+    transports: ["websocket", "polling"]
 });
 
 export default function LiveChat() {
